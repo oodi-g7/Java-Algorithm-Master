434b0cb553bff1f267479176420a1c6ce61fa04d
oodi-g7 <oodig7@gmail.com>
2023-06-27 00:38:46 +0900

    0627 step01.입출력

---
+++ src/main/java/com/study/javaalgorithm/baekjoon/step01/n1000.java
@@ -0,0 +1,17 @@
+package com.study.javaalgorithm.baekjoon.step01;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class n1000 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String[] str = (br.readLine()).split(" ");
+        int a = Integer.parseInt(str[0]);
+        int b = Integer.parseInt(str[1]);
+
+        System.out.println(a+b);
+    }
+}
--- src/main/java/com/study/javaalgorithm/baekjoon/week01/n1000.java
+++
@@ -1,7 +0,0 @@
-package com.study.javaalgorithm.baekjoon.week01;
-
-public class n1000 {
-    public static void main(String[] args) {
-
-    }
-}
