6323437a210be60bbe6c8e6dad08430cb1532a8a
oodi-g7 <oodig7@gmail.com>
2023-06-28 14:43:45 +0900

    230628 : n10951

--- src/main/java/com/study/javaalgorithm/baekjoon/step01/n10951.java
+++ src/main/java/com/study/javaalgorithm/baekjoon/step01/n10951.java
@@ -1,14 +1,27 @@
 package com.study.javaalgorithm.baekjoon.step01;
 
 import java.io.*;
+import java.util.StringTokenizer;
 
 public class n10951 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st;
+        String str;
 
-        String s = br.readLine();
+        while( (str=br.readLine()) != null ){
+            st = new StringTokenizer(str, " ");
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            int sum = a+b;
+            bw.write(String.valueOf(sum)+"\n"); // 개행 추가해줘야하는 이유 ...?
+        }
 
-        System.out.println(s);
+        bw.flush();
+
+        br.close();
+        bw.close();
     }
 }
+// https://st-lab.tistory.com/40
