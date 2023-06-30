6d2f5aed2e03a59c219dc09084777c2e0eb99d0b
oodi-g7 <oodig7@gmail.com>
2023-06-30 16:57:08 +0900

    230630 : n10953 resolving

--- src/main/java/com/study/javaalgorithm/baekjoon/step01/n10953.java
+++ src/main/java/com/study/javaalgorithm/baekjoon/step01/n10953.java
@@ -1,26 +1,35 @@
 package com.study.javaalgorithm.baekjoon.step01;
 
 import java.io.*;
-import java.util.StringTokenizer;
+import java.util.Scanner;
 
 public class n10953 {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        int cnt = scn.nextInt();
+
+        for (int i=0; i<cnt; i++) {
+            String str = scn.next();
+            int a = Integer.parseInt(str.split(",")[0]);
+            int b = Integer.parseInt(str.split(",")[1]);
+
+            System.out.println(a + b);
+        }
+    }
+
+    public void case_one() throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         int cnt = Integer.parseInt(br.readLine());
 
-        String str = br.readLine();
-        StringTokenizer st = new StringTokenizer(str, ",");
-
-        while(cnt-1 > 0){
-            int a = Integer.parseInt(st.nextToken());
-            int b = Integer.parseInt(st.nextToken());
+        for (int i=0; i<cnt; i++){
+            String s = br.readLine();
+            int a = Integer.parseInt(s.split(",")[0]);
+            int b = Integer.parseInt(s.split(",")[1]);
             String sum = String.valueOf(a+b);
 
             bw.write(sum+"\n");
-            cnt--;
         }
-
         bw.flush();
 
         bw.close();
