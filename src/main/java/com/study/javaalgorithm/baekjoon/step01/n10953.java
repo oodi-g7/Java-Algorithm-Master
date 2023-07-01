d3fa97c256004d69eaddcc2471b6b615c8ebbbb4
oodi-g7 <oodig7@gmail.com>
2023-07-01 22:56:13 +0900

    230701 : n10953

--- src/main/java/com/study/javaalgorithm/baekjoon/step01/n10953.java
+++ src/main/java/com/study/javaalgorithm/baekjoon/step01/n10953.java
@@ -4,20 +4,7 @@ import java.io.*;
 import java.util.Scanner;
 
 public class n10953 {
-    public static void main(String[] args) {
-        Scanner scn = new Scanner(System.in);
-        int cnt = scn.nextInt();
-
-        for (int i=0; i<cnt; i++) {
-            String str = scn.next();
-            int a = Integer.parseInt(str.split(",")[0]);
-            int b = Integer.parseInt(str.split(",")[1]);
-
-            System.out.println(a + b);
-        }
-    }
-
-    public void case_one() throws IOException{
+public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         int cnt = Integer.parseInt(br.readLine());
