0f2212d8aa5d0088b3523a7c007674bde7f53222
oodi-g7 <oodig7@gmail.com>
2023-07-06 16:49:00 +0900

    230706 : n10953

--- src/main/java/com/study/javaalgorithm/baekjoon/step01/n10953.java
+++ src/main/java/com/study/javaalgorithm/baekjoon/step01/n10953.java
@@ -2,9 +2,66 @@ package com.study.javaalgorithm.baekjoon.step01;
 
 import java.io.*;
 import java.util.Scanner;
+import java.util.StringTokenizer;
 
 public class n10953 {
-    public static void main(String[] args) {
-        
+    public static void main0(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st;
+        String str;
+
+        int cnt = Integer.parseInt(br.readLine());
+
+        for (int i=0; i<cnt; i++){
+            str = br.readLine();
+            st = new StringTokenizer(str, ",");
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+
+            String sum = String.valueOf(a+b);
+
+            if (i == cnt-1){
+                bw.write(sum);
+            }else {
+                bw.write(sum + "\n");
+            }
+        }
+        bw.flush();
+
+        br.close();
+        bw.close();
+    }
+
+    public static void main2(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int cnt = Integer.parseInt(br.readLine());
+
+        StringBuilder sb = new StringBuilder();
+
+        for (int i=0; i<cnt; i++){
+            StringTokenizer st = new StringTokenizer(br.readLine(), ",");
+            sb.append(Integer.parseInt(st.nextToken()) + Integer.parseInt(st.nextToken()) + "\n");
+        }
+        br.close();
+
+        System.out.println(sb.toString());
+    }
+    // https://onlyfor-me-blog.tistory.com/317
+
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        String str;
+
+        int cnt = Integer.parseInt(br.readLine());
+        for (int i=0; i<cnt; i++){
+            str = br.readLine();
+            sb.append((str.charAt(0)-'0') + (str.charAt(2)-'0')).append("\n");
+        }
+
+        System.out.println(sb.toString());
+
+        br.close();
     }
 }
