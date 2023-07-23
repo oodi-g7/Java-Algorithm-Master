874710c751bdd117769007bf1fbf5883deda2314
oodi-g7 <oodig7@gmail.com>
2023-07-23 22:48:42 +0900

    n11718

--- src/main/java/com/study/javaalgorithm/baekjoon/step01/n11718_n11719.java
+++ src/main/java/com/study/javaalgorithm/baekjoon/step01/n11718_n11719.java
@@ -2,8 +2,12 @@ package com.study.javaalgorithm.baekjoon.step01;
 
 import java.io.*;
 
-// 230717 : 14224KB | 128ms
-public class n11718 {
+
+public class n11718_n11719 {
+    // [n11718] 230717 : 14224KB | 128ms
+    // [n11719] 230717 : 14168KB | 124ms
+    // 11718에서는 입력값에 공백이 없었는데, 11719에서는 공백에 추가됨.
+    // 그러나 ***BufferedReader는 공백 또한 값으로 인식***하므로, != null 조건에 부합함.
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringBuilder sb = new StringBuilder();
