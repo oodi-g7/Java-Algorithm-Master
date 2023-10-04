2180564239ee829559160e95e5c43c3081df5626
oodi-g7 <oodig7@gmail.com>
2023-10-05 00:01:48 +0900

    day1_231006

--- src/main/java/com/study/javaalgorithm/baekjoon/nh_cote/no_003.java
+++ src/main/java/com/study/javaalgorithm/baekjoon/nh_cote/no_003.java
@@ -8,8 +8,8 @@ import java.util.StringTokenizer;
 
 public class no_003 {
     public static void main(String[] args) throws IOException {
-        // 구간합 공식 => S[i] = S[i-1] + S[i]
-        // i ~ j까지 부분합 공식 => S[j] - S[i-i]
+        // 구간합 공식 : S[i] = S[i-1] + S[i]
+        // i ~ j까지 부분합 공식 : S[j] - S[i-i]
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
 
